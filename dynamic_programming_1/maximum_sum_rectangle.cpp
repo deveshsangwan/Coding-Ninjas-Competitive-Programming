27abc8dc674d7938bf3a6f99804c53a7a449607e
deveshsangwan <dev.sangwan2001@gmail.com>
2020-08-18 21:35:07 +0530

    added questions

--- dynamic_programming_1/maximum_sum_rectangle.cpp
+++ dynamic_programming_1/maximum_sum_rectangle.cpp
@@ -19,23 +19,16 @@ Sample Output
 #include<bits/stdc++.h>  
 using namespace std;  
 
-// Implementation of Kadane's algorithm for  
-// 1D array. The function returns the maximum  
-// sum and stores starting and ending indexes  
-// of the maximum sum subarray at addresses  
-// pointed by start and finish pointers  
-// respectively.  
+  
 int kadane(int* arr, int* start, 
            int* finish, int n)  
 {  
-    // initialize sum, maxSum and  
+      
     int sum = 0, maxSum = INT_MIN, i;  
   
-    // Just some initial value to check 
-    // for all negative values case  
+     
     *finish = -1;  
   
-    // local variable  
     int local_start = 0;  
   
     for (i = 0; i < n; ++i)  
@@ -54,17 +47,14 @@ int kadane(int* arr, int* start,
         }  
     }  
   
-    // There is at-least one  
-    // non-negative number  
+     
     if (*finish != -1)  
         return maxSum;  
   
-    // Special Case: When all numbers 
-    // in arr[] are negative  
+
     maxSum = arr[0];  
     *start = *finish = 0;  
-  
-    // Find the maximum element in array  
+    
     for (i = 1; i < n; i++)  
     {  
         if (arr[i] > maxSum)  
@@ -76,45 +66,32 @@ int kadane(int* arr, int* start,
     return maxSum;  
 }  
   
-// The main function that finds 
-// maximum sum rectangle in M[][]  
-void findMaxSum(int **M, int ROW, int COL)  
+ 
+ void findMaxSum(int **M, int ROW, int COL)  
 {  
-    // Variables to store the final output  
     int maxSum = INT_MIN, finalLeft, finalRight,  
                           finalTop, finalBottom;  
   
     int left, right, i;  
     int temp[ROW], sum, start, finish;  
   
-    // Set the left column  
+     
     for (left = 0; left < COL; ++left)  
     {  
-        // Initialize all elements of temp as 0  
+          
         memset(temp, 0, sizeof(temp));  
   
-        // Set the right column for the left 
-        // column set by outer loop  
         for (right = left; right < COL; ++right)  
         {  
               
-            // Calculate sum between current left  
-            // and right for every row 'i'  
+              
             for (i = 0; i < ROW; ++i)  
                 temp[i] += M[i][right];  
   
-            // Find the maximum sum subarray in temp[].  
-            // The kadane() function also sets values   
-            // of start and finish. So 'sum' is sum of  
-            // rectangle between (start, left) and  
-            // (finish, right) which is the maximum sum  
-            // with boundary columns strictly as left  
-            // and right.  
+              
             sum = kadane(temp, &start, &finish, ROW);  
   
-            // Compare sum with maximum sum so far.  
-            // If sum is more, then update maxSum and  
-            // other output values  
+            
             if (sum > maxSum)  
             {  
                 maxSum = sum;  
@@ -129,7 +106,7 @@ void findMaxSum(int **M, int ROW, int COL)
     cout << maxSum << endl;  
 }  
   
-// Driver Code 
+
 int main()  
 {
     int ROW, COL;
